b9bed34785715f5cf5ea52f7a8c3ac487e6e342c
Guilherme Souza <7guilhermesouza@gmail.com>
2025-05-16 23:32:53 -0300

    Alteraçao da pagina indice para deploy

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 function Home() {
-  return;
-  <div>
-    <h1>Nayara, nosso primeiro site está online.</h1>;
-    <h3>Aaaaaaaah e eu não poderia deixar de dizer. Eu te amo!</h3>
-  </div>;
+  return (
+    <div>
+      <h1>
+        Atenção! esta é uma mensagem secreta e vale um beijo para uma morena
+        linda e cheirosa.
+      </h1>
+      <p>Mas, ela só pode receber se der um sorrisinho rsrs</p>
+    </div>
+  );
 }
 
 export default Home;

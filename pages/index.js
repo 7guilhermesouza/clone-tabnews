57fee3bf877813fdf8d0102a9c2fa41f9180dabd
Guilherme Souza <7guilhermesouza@gmail.com>
2025-05-16 23:16:16 -0300

    inclui a página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Título da página principal</h1>;
+  return;
+  <div>
+    <h1>Nayara, nosso primeiro site está online.</h1>;
+    <h3>Aaaaaaaah e eu não poderia deixar de dizer. Eu te amo!</h3>
+  </div>;
 }
 
 export default Home;

9c80b4ab152ea9e231b7f103b24b66df99f04fe4
Guilherme Souza <7guilhermesouza@gmail.com>
2026-08-24 21:06:08 -0300

    implement a method to return a opened connection with db, alter tests scripts and alter folder structure

---
+++ infra/database.js
@@ -0,0 +1,39 @@
+import { Client } from "pg";
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV == "production" ? true : false;
+}
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+  client.connect();
+  return client;
+}
+async function query(queryObject) {
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    client.end();
+  }
+}
+
+export default { query, getNewClient };

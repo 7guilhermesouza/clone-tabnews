e549a93c6984116e6b6e0082bfe3591fab689edb
Guilherme Souza <7guilhermesouza@gmail.com>
2026-08-24 21:11:37 -0300

    add `/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,13 @@
+import database from "infra/database.js";
+
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+}
+beforeAll(cleanDatabase);
+
+test("GET to /migrations should be return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  const responseBody = await response.json();
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
